3f57c6cddcba3c0a3491af25fbd4259c777aadf8
stylopm <apmurillo99@hotmail.com>
2022-06-22 00:13:10 -0500

    Se agrega la parte de la grafica

---
+++ ejemplo01/script.js
@@ -0,0 +1,29 @@
+console.log('Graficas')
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
---
+++ ejemplo02/script.js
@@ -0,0 +1,31 @@
+console.log('Graficas')
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+
+const myChart = new Chart(ctx, config);
