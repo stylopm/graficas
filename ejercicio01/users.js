4d8b5f4fff56ad4e217b7b3b6e7ca3acef31bde0
stylopm <apmurillo99@hotmail.com>
2022-06-22 00:16:29 -0500

    feat: add soluctions

---
+++ ejercicio01/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {"id": 1, "name":"John","phone": 300239485},
+    {"id": 2, "name":"Marie","phone": 245676430},
+    {"id": 3, "name":"Ana","phone": 567836330},
+    {"id": 4, "name":"David","phone": 49654730},
+    {"id": 5, "name":"Jessica","phone":2536780},
+    {"id": 6, "name": "James","phone": 389876650},
+    {"id": 7, "name": "Henry", "phone": 78965432},
+    {"id": 8, "name": "Tony", "phone": 98765432},
+    {"id": 9, "name": "Marcus", "age": 30},
+]
+module.exports = users;
